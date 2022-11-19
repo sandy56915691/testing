a1690666808b2a04f3c7f5eed46131412b9b508a
Neetu <Neetu@Neetu-PC>
2022-11-19 22:36:49 +0530

    Next Commit

--- src/main/java/randomPackage/RandomClass.java
+++ src/main/java/randomPackage/RandomClass.java
@@ -1,21 +1,12 @@
 package randomPackage;
 
-import java.util.Arrays;
+import java.security.KeyStoreException;
 
-import org.testng.annotations.Test;
-
-@Test()
-public class RandomClass {
+public class RandomClass {	
 	
-	@Test()
-	public void m1() {
-		
-		int[] a = {1,2,3,4,5};
-		int[] b = a;
-		b[1]=10;
-		System.out.println(Arrays.toString(a));
-		
-	}
+
+	public static void main(String[] args) throws KeyStoreException {
 	
 	}
+}
 
